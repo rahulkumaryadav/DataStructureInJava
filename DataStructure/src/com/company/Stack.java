cb1e81d2f6de06d7f00d0520029ced9f4ab1e98c
Rahul kumar <rahulkumar96369@gmail.com>
2017-09-09 15:59:13 +0100

    this is first commit for stack

---
+++ DataStructure/src/com/company/Stack.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by DELL on 9/9/17.
+ */
+public class Stack {
+    public static void main(String[] args){
+        System.out.println("this is the stack project");
+    }
+}
